e4204630f7c2d381fdc203bdfa7a922f509296a5
Vit Onischenko <slonik0775@gmail.com>
2022-09-05 03:12:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useState} from 'react';
+import {Block} from './Block';
+import './index.scss';
+
+function App() {
+    const [fromCurrency, setFromCurrency] = useState('UAH')
+    const [toCurrency, setToCurrency] = useState('USD')
+    const [fromPrice, setFromPrice] = useState(0)
+    const [toPrice, setToPrice] = useState(1)
+
+    // const [rates, setRates] = useState({})
+const ratesRef = React.useRef({})
+
+    useEffect(() => {
+        fetch('https://cdn.cur.su/api/latest.json')
+            .then((res) => res.json())
+            .then((json) => {
+                // setRates(json.rates);
+                ratesRef.current = json.rates
+onChangeToPrice(1)
+            })
+            .catch((err) => {
+                console.warn(err);
+                alert('Не удалось получить информацию')
+            })
+    }, [])
+
+
+    const onChangeFromPrice = (value) => {
+        const price = value /  ratesRef.current [fromCurrency]
+        const result = price *  ratesRef.current [toCurrency]
+        setFromPrice(value)
+        setToPrice(result.toFixed(3))
+    }
+
+    const onChangeToPrice = (value) => {
+        const result =  ratesRef.current [fromCurrency] /  ratesRef.current [toCurrency] * value
+        setFromPrice(result.toFixed(3))
+        setToPrice(value)
+    }
+
+    React.useEffect(() => {
+        onChangeFromPrice(fromPrice)
+    }, [fromCurrency])
+
+
+    React.useEffect(() => {
+        onChangeToPrice(toPrice)
+    }, [toCurrency])
+
+
+    return (
+        <div className="App">
+            <Block value={fromPrice}
+                   currency={fromCurrency}
+                   onChangeCurrency={setFromCurrency}
+                   onChangeValue={onChangeFromPrice}/>
+            <Block value={toPrice}
+                   currency={toCurrency}
+                   onChangeCurrency={setToCurrency}
+                   onChangeValue={onChangeToPrice}/>
+        </div>
+    );
+}
+
+
+export default App;
